0b9c9616ff2b4406e3a1a136575c95157d8bcb5e
Alex Dunia <thealexdunia@gmail.com>
2023-08-03 04:13:01 +0100

    fixed countdown, nav and login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,13 @@
 <body>
     {{-- Navigation panel --}}
 
+
     <div class="ctna">
+        <div class="lisu">
+            <h3> <a> Log in </a> </h3>
+        </br>
+            <h3> <a> Sign up </a>  </h3>
+        </div>
         <div class="navflex">
           <div class="flogow">
             <img src="/images/tdlogo.png" alt="Logo"/>
@@ -23,17 +29,35 @@
               <li><a> Music </a></li>
               <li><a> Movies </a></li>
               <li><a> Theatre/Comedy</a></li>
-              <li><a> Sportss </a></li>
+              <li><a> Sports </a></li>
               <li><a> Contact us </a></li>
             </ul>
           </div>
-          <div>
-            <button class="btn">
+          <div class="userandcart">
+              <div class="circle">
+                <span class="number">1</span>
+                <i class="fa-solid fa-cart-shopping"></i>
+              </div>
+
+              {{-- <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="circle-user" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-circle-user fa-xl"><path fill="currentColor" d="M406.5 399.6C387.4 352.9 341.5 320 288 320H224c-53.5 0-99.4 32.9-118.5 79.6C69.9 362.2 48 311.7 48 256C48 141.1 141.1 48 256 48s208 93.1 208 208c0 55.7-21.9 106.2-57.5 143.6zm-40.1 32.7C334.4 452.4 296.6 464 256 464s-78.4-11.6-110.5-31.7c7.3-36.7 39.7-64.3 78.5-64.3h64c38.8 0 71.2 27.6 78.5 64.3zM256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-272a40 40 0 1 1 0-80 40 40 0 1 1 0 80zm-88-40a88 88 0 1 0 176 0 88 88 0 1 0 -176 0z" class=""></path></svg> --}}
+
+              <div class="user" id="usericonid">
+                 <i class="fa fa-circle-user"></i>
+                {{-- <i class="fa fa-circle-user"> <span class="v"> v </span> </i> --}}
+              </div>
+
+              <div class="user" id="cancelpop">
+                <i class="fa-solid fa-x"></i>
+               {{-- <i class="fa fa-circle-user"> <span class="v"> v </span> </i> --}}
+             </div>
+
+            {{-- <button class="btn">
               My cart
-            </button>
+            </button> --}}
           </div>
         </div>
       </div>
+
 
 
     {{-- End of Nav bar --}}
@@ -170,6 +194,22 @@ function showSlides() {
     setTimeout(showSlides, 5000); // Change image every 2 seconds (adjust as needed)
 }
 
+// Cancel pop up
+const cpop = document.getElementById("cancelpop");
+const uid = document.getElementById("usericonid");
+const lisu = document.querySelector(".lisu");
+
+cpop.addEventListener("click", function() {
+    lisu.style.display = "none";
+    uid.style.display = "block";
+    cpop.style.display = "none";
+});
+
+uid.addEventListener("click", function() {
+    lisu.style.display = "block";
+    uid.style.display = "none";
+    cpop.style.display = "block";
+});
 </script>
 
 
